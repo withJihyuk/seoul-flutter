598e26a3fee9c2d3a6bcd414e36b06bd389c6446
parkyuhyeon <pyh1111802@naver.com>
2024-05-07 01:44:36 +0900

    ➕ :: 카카오맵 적용

--- lib/views/map/map_page.dart
+++ lib/views/map/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -6,7 +7,7 @@ class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SafeArea(child: Text("Map")),
+      body: KakaoMap(),
     );
   }
 }
